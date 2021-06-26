4fe3cc9a99f4198a2de19ff7d2ce24e536ece3a8
IsraelFM <israelfmoraes@outlook.com>
2021-06-26 17:03:29 -0300

    Add navigation

--- App.js
+++ App.js
@@ -10,13 +10,10 @@ import {
 } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
+
 import { default as theme } from './theme.json';
 import { default as mapping } from './mapping.json';
 
-/**
- * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
- * https://akveo.github.io/eva-icons
- */
 const HeartIcon = (props) => (
 	<Icon {...props} name='heart' />
 );
